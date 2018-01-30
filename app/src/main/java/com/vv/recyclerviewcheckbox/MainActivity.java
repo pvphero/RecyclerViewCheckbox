1768ee76d4786464344543b489dcb166481a9d87
shenzhenwei <impvphero@163.com>
2018-01-30 18:06:48 +0800

    first

--- app/src/main/java/com/vv/recyclerviewcheckbox/MainActivity.java
+++ app/src/main/java/com/vv/recyclerviewcheckbox/MainActivity.java
@@ -3,14 +3,20 @@ package com.vv.recyclerviewcheckbox;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
+    private RecyclerView recyclerView;
+    private View deleteButton;
+    private View cancelButton;
+    private CheckBox selectAllCheckbox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView=findViewById(R.id.rv_list);
+
     }
 }
